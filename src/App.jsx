400f1278eb420df1873bad64aa6ccf2898705468
iamRichardCN <iamrichardcn@gmail.com>
2025-06-21 14:22:11 +0100

    Initial commit - React ML application

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import { useEffect, useState} from "react"
+
+
+
+const Card = ({title}) => {
+    const [count, setCount] = useState(0);
+    const [hasLiked, setHasLiked] = useState( false);
+
+    useEffect(() => {
+        console.log(`${title} has been liked: ${hasLiked}`);
+    },[hasLiked]);
+
+
+    return (
+        <div className="card" onClick={()=> setCount(count +1)}>
+            <h2> {title} <br/> {count || null }</h2>
+            <button onClick={()=>setHasLiked( !hasLiked)}>
+                {hasLiked ? '❤️ ': '🤍 '}
+            </button>
+        </div>
+
+    )
+}
+
+const App = () => {
+    return (
+        <div className="card-container">
+            <Card title={"star wars"} />
+            <Card title={"avatar"}/>
+            <Card title={"The lion King"}/>
+
+        </div>
+    )
+}
+
+export default App
